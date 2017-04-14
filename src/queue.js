7d5fb38b1443880cf19291545075aa1b0d43057f
Parfait <jenny.filcheck@gmail.com>
2017-04-13 23:03:21 -0500

    finsihed stack/queue/linked-list

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,19 @@
 class Queue {
+  constructor() {
+    this.queue = [];
+  }
 
+  get size() {
+    return this.queue.length;
+  }
+
+  enqueue(obj) {
+    this.queue.push(obj);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,19 @@
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
 
+  get size() {
+    return this.stack.length;
+  }
+
+  add(obj) {
+    this.stack.push(obj);
+  }
+
+  remove() {
+    return this.stack.pop();
+  }
 }
 
 module.exports = Stack;
