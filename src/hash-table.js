3ceb64bd1b9f29e4dadeb05b7e8be9b807d30c1d
hohguy <dfourney@gmail.com>
2017-04-15 15:16:13 -0400

    Hash table solution.

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,21 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+
+  insert(key, value) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    this.storage.set(index, value);
+  }
+
+  retrieve(key) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    return (this.storage.get(index));
+  }
+
+  remove(key) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    this.storage.set(index, null);
+  }
 }
 
 module.exports = HashTable;
