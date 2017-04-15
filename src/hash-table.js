443d14cb276b93e2701c529e18b99d49c04ee8e8
hohguy <dfourney@gmail.com>
2017-04-15 19:38:27 -0400

    Hashtable rebalancing first attempt.

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
-const LinkedList = require('./linked-list');
 
 class HashTable {
   constructor() {
@@ -9,12 +8,50 @@ class HashTable {
     // Do not modify anything inside of the constructor
   }
 
+  resize() {
+    const newLimit = (this.limit * 2);
+    const newArray = new LimitedArray(newLimit);
+
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage) {
+        const valuesArray = this.storage[i];
+        for (let j = 0; j < valuesArray.length; j++) {
+          const key = valuesArray[j].key;
+          const value = valuesArray[j].value;
+          const index = getIndexBelowMax(valuesArray[j].key.toString(), newLimit);
+          const currentValueArr = newArray.get(index);
+
+          if (!currentValueArr) {
+            newArray.set(index, [{ key, value }]);
+          } else {
+            for (let k = 0; k < currentValueArr.length; k++) {
+              if (currentValueArr[k].value === value) return;
+            }
+            currentValueArr.push({ key, value });
+            newArray.set(index, currentValueArr);
+          }
+        }
+      }
+    }
+
+    this.storage = newArray;
+    this.limit = newLimit;
+  }
+
   insert(key, value) {
     // if two keys map to the same index in the storage, store a 2nd array as the value
     // make each key/value its own array that is nested inside of the array stored at that
     // index on the table
     const index = getIndexBelowMax(key.toString(), this.limit);
     const currentValueArr = this.storage.get(index);
+
+    // check if we need to resize the hash table
+    // current max fill is 75%
+    let fullCells = 1;
+    this.storage.each((bucket) => {
+      if (bucket !== undefined) fullCells++;
+    });
+    if (fullCells / this.limit > 0.75) this.resize();
 
     if (!currentValueArr) {
       this.storage.set(index, [{ key, value }]);
